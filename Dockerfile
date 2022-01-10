e529c041b1d9eee1147d48fea2de123a6eb391a4
Alefe Patrick <alefepdias@gmail.com>
2022-01-09 20:47:45 -0400

    alterações

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM nginx:1.17.1-alpine
-COPY /dist/tqiEvolution /usr/share/nginx/html
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const appName = 'tqiEvolution'
 
-app.use(express.static(__dirname + '/dist/tqiEvolution'));
+app.use(express.static(__dirname + `/dist/${appName}`))
 
-app.get('/*', (req,res) => {
-  res.sendFile(__dirname + '/dist/tqiEvolution/index.html');
+app.get('/*', function (req, res){
+      res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`))
 });
 
-app.listen(PORT, () => {
-  console.log('Servidor iniciado na porta ' + PORT);
-})
+app.listen(process.env.PORT || 8080)
